185d25f28879fee6b566487ae99d6e4ce3b6318f
aajdr <aarondudedrake@gmail.com>
2017-10-02 09:45:23 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,14 @@
 
 int main()
 {
+  int story = 1;
   bool done = false;
     while(done == false)
-    { if(int story == 1)
+    { if(story == 1)
     {
-      std::cout << "Once upon a time" << std::cout
+      std::cout << "Once upon a time" << std::cout;
       }
-  std::cout << "hi there Jackson" << std::endl
+  std::cout << "hi there Jackson" << std::endl;
+    }
+  bool done = true;
 }
